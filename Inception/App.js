fe39d9bf28a04840495c538b27a9a02c9ad28b4d
jayesh <jayeshdalal7@gmail.com>
2023-08-19 10:51:31 +0530

    update app.js with more details.

--- Inception/App.js
+++ Inception/App.js
@@ -1,8 +1,36 @@
-const heading = React.createElement(
-    "h1", 
-    {id:"heading",'data-cy':"h1_heading" }, 
-    "Hello Jayesh from React");
+// const heading = React.createElement(
+//     "h1", 
+//     {id:"heading",'data-cy':"h1_heading" }, 
+//     "Hello Jayesh from React");
 
-console.log(heading); // heading : returns object
-const root = ReactDOM.createRoot(document.getElementById("root"));  
-root.render(heading); // render the heading in the root element
+// console.log(heading); // heading : returns object
+// const root = ReactDOM.createRoot(document.getElementById("root"));  
+// root.render(heading); // render the heading in the root element
+
+
+// nested element
+
+/*
+  <div id = "parent">
+    < div id = "child">
+        <h1 id = "heading"> Hello Jayesh from React </h1>
+    </div>
+   </div>
+
+   ReactElement(object) -> ReactElement(object) -> ReactElement(object)
+
+*/
+
+const parent = React.createElement(
+    "div", {id:"parent"}, 
+    React.createElement(
+        "div", {id:"child"},
+       [ React.createElement(
+            "h1", {}, "I am an h1 element") , 
+        React.createElement(
+                "h2", {}, "I am an h2 element")]
+            
+));
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
